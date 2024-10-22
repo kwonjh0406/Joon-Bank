a634efc3803a60679268d695d6b1f919e75fe70c
kwonjh0406 <kwonjh0406@gmail.com>
2024-10-22 23:55:55 +0900

    chore: Spring Boot 프로젝트 생성

---
+++ src/main/java/com/kwonjh0406/joon_bank/JoonBankApplication.java
@@ -0,0 +1,13 @@
+package com.kwonjh0406.joon_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoonBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoonBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwonjh0406/joon_bank/JoonBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwonjh0406.joon_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoonBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
