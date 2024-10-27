7832e79282483dced708f08bc644a43d7ae52447
kwonjh0406 <kwonjh0406@gmail.com>
2024-10-27 16:41:47 +0900

    feat: Security Config 추가

---
+++ src/main/java/com/kwonjh0406/joon_bank/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.kwonjh0406.joon_bank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/login").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/my/**").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
